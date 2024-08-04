d8babc40bac49bf68c9696f741991c1b398f72e0
Omid Tahmasebi <tomid7373@gmail.com>
2024-08-04 04:54:22 +0330

    commit (1)

---
+++ quotes.script.js
@@ -0,0 +1,30 @@
+var i = 0;
+
+function changeQuote()
+{
+    var quotes = document.getElementById('center').getElementsByTagName('p') ;
+    i++ ;
+    if(i == quotes.length)
+        i = 0;
+
+    if(i==0)
+        quotes[quotes.length-1].style.display = 'none' ;
+    else
+        quotes[i-1].style.display = 'none' ;
+
+    quotes[i].style.display = 'block' ;
+}
+
+var textColor = []
+var index = 0
+function changeBackground()
+{
+    var page = document.getElementById('container') ;
+    var backgrounds = ['cyan' , 'tomato' , 'crimson' , 'green' , 'blue'] ;
+    index++ ;
+
+    if(index==backgrounds.length)
+        index = 0
+    
+    page.style.backgroundColor = backgrounds[index]
+}
